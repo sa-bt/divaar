5d6977b95e301a40db9a3f016687e2107119c41d
sa-bt <bakhshian2020@gmail.com>
2022-07-12 23:01:46 +0430

    design login page

--- Modules/Core/Resources/views/layouts/app.blade.php
+++ Modules/Core/Resources/views/layouts/app.blade.php
@@ -1,13 +1,14 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fa_IR" dir="rtl">
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="{{mix('css/tailwind.css')}}">
+    <link rel="stylesheet" href="{{asset('css/font/font.css')}}">
     @stack('head')
 </head>
-<body>
+<body style="font-family: irs" class="select-none text-gray-800">
 
 @stack('before-content')
 
--- Modules/Otp/Resources/views/login.blade.php
+++ Modules/Otp/Resources/views/login.blade.php
@@ -1,9 +1,24 @@
 @extends('core::layouts.app')
 
 @section('content')
-    <h1 class="bg-green-800 ">Salam World</h1>
+    <section class="w-full h-screen flex flex-col ">
+        <div class="flex items-center justify-between w-full p-4 shadow">
+            <h1 class="text-lg font-bold">ورود به حساب کاربری</h1>
+            <span>x</span>
+        </div>
 
-    <p>
-        This view is loaded from module: {!! config('otp.name') !!}
-    </p>
+
+        <div class="mt-6 px-4">
+            <h1> شماره موبایل خود را وارد کنید</h1>
+            <p class="mt-4 text-sm text-gray-500">در وارد کردن اطلاعات دقت فرمایید. کد تایید به شماره وارد شده در کادر زیر ارسال می گردد</p>
+            <label class="relative">
+                <input type="tel" class="mt-4 w-full rounded-md bg-gray-100 border
+                border-rose-100 border-opacity-0 outline-none pt-1.5 pb-1 pl-12 pr-28
+                text-gray-600 transition transition-all duration-200 focus:border-opacity-100 "
+                       maxlength="11" dir="ltr">
+                <span class="absolute left-4 text-gray-500" dir="ltr">+98</span>
+                <span class="absolute right-4 text-gray-500" dir="ltr">شماره موبایل</span>
+            </label>
+        </div>
+    </section>
 @endsection
